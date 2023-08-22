39f33668bc783744b2c6dd597e3529688398b63e
unknown <edgarerb@hotmail.es>
2023-08-22 15:22:48 +0200

    tengo una idea

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,17 @@
+export const alumnos = [
+    {
+        nombre: 'Ricky',
+        apellido: 'Perea',
+        promedio: 4.5
+    },
+    {
+        nombre: 'Eusebio',
+        apellido: 'Gonzalez',
+        promedio: 6.5
+    },
+    {
+        nombre: 'Wilfredo',
+        apellido: 'el Velloso',
+        promedio: 9
+    },
+]
---
+++ src/app/components/tabla-cursos/mock.ts
@@ -0,0 +1,17 @@
+export const cursos = [
+    {
+        materia: 'Literatura',
+        dificultad: 4,
+        requiere: 'ganas'
+    },
+    {
+        materia: 'Matemáticas',
+        dificultad: 7,
+        requiere: 'concentración'
+    },
+    {
+        materia: 'Angular',
+        dificultad: 9,
+        requiere: 'paciencia'
+    },
+]
